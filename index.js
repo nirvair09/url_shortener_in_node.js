d8b60794f72bfd5704942e616f273cdaddcb718d
nirvair09 <chandradeep262003@gmail.com>
2025-10-30 10:41:17 +0530

    post method added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const dotenv=require("dotenv");
 const { default: connectToDB } = require("./db");
+const { default: longToShortURL } = require("./url.modal");
 
 dotenv.config();
 
@@ -10,6 +11,51 @@ connectToDB();
 
 app.get("/",(req,res)=>{
     res.send("Hello from url shortener");
+})
+
+
+app.post("/",async(req,res)=>{
+    try {
+
+        const {name,url} = req.body||null;
+
+        if(!name || !url){
+            return res.status(400).json({
+                status:"Failed",
+                message:"Name and URL must be provided",
+            });
+        };
+
+        const timeStamp=Date.now();
+
+        const mongoData={
+            name,
+            LongURL:url,
+            shortURL:"http://short.ly/"+(name+timeStamp).toString(),
+        }
+
+        const doc = await longToShortURL.create(mongoData);
+
+        if(!doc){
+            return res.status(400).json({
+                status:"Failed creation of url at dataBase",
+                message:"Error occured while creating short URL",
+            });
+        }else{
+            return res.status(200).json({
+                status:"Success",
+                message:"Short URL created successfully",
+                shortURL:doc.shortURL,
+            })
+        }
+        
+    } catch (error) {
+        console.log("Error",error);
+        res.status(500).json({
+            status:"Failed",
+            message:"Internal Server Error",
+        })
+    }
 })
 
 app.listen( process.env.PORT  ,()=>{
